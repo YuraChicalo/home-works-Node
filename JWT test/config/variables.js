35007d2ecb54be474c5e70058c9e57cdb49dda82
YuraChicalo <47437645+YuraChicalo@users.noreply.github.com>
2021-09-23 00:28:31 +0300

    Delete JWT test directory

--- JWT test/config/variables.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT: process.env.port || 5000,
-    DB_CONNECTION: process.env.DB_CONNECTION || 'mongodb://localhost:27017/apr-2021',
-    current_year: new Date().getFullYear(),
-    ACCESS_SECRET_KEY: 'Secret',
-    REFRESH_SECRET_KEY: 'S2',
-    AUTHORIZATION: 'Authorization'
-};
